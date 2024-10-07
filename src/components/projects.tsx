7949b0f3f28760d2b4176636d57640c1abed246b
remon-rakibul <remon.rakibul.star@gmail.com>
2024-10-07 09:04:28 +0600

    fix: project links

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,3 +1,4 @@
+import { title } from "framer-motion/client";
 import Card from "./card";
 // import projectImg1 from "../assets/projects/projectImg1.png";
 
@@ -7,71 +8,78 @@ const Projects = () => {
       title: "ARChat",
       description:
         "Custom chatbot creation with multiple knowledge bases, optimized for retrieval-augmented generation (RAG) based question answering.",
-      primaryUrl: "https://arc-chat.vercel.app/",
-      secondaryUrl: "https://github.com/remon-rakibul/autonomous-hr", // Optional, if applicable
+      primaryUrl: "#",
+      secondaryUrl: "https://arc-chat.vercel.app/", // Optional, if applicable
     },
     {
       title: "AutoHR",
       description:
         "An autonomous multi-agent system that automates most workflows for an HR persona, including the ability to pull, research, and draft email responses.",
       primaryUrl: "https://github.com/remon-rakibul/autonomous-hr",
-      secondaryUrl: "https://github.com/remon-rakibul/autonomous-hr", // Optional, if applicable
+      secondaryUrl: "#", // Optional, if applicable
     },
     {
       title: "CV-Sorting",
       description:
         "A Proof of Concept (PoC) project that sorts resumes against job descriptions using relevance scoring.",
-      primaryUrl: "https://github.com/remon-rakibul/resume-categorization-ml",
-      secondaryUrl: "https://github.com/remon-rakibul/resume-categorization-ml", // Optional, if applicable
+      primaryUrl: "#",
+      secondaryUrl: "#", // Optional, if applicable
     },
     {
       title: "AI Study Buddy",
       description:
         "A question generation and answering platform for students and teachers, capable of generating questions and answers based on provided documents.",
-      primaryUrl: "https://ai-study-buddy-qxb2jcy4zaqmzcuuryedhl.streamlit.app/",
-      secondaryUrl: "https://github.com/remon-rakibul/AI-Study-Buddy", // Optional, if applicable
+      primaryUrl: "https://github.com/remon-rakibul/AI-Study-Buddy",
+      secondaryUrl: "https://ai-study-buddy-qxb2jcy4zaqmzcuuryedhl.streamlit.app/", // Optional, if applicable
     },
     {
       title: "Chatbot AI Backend",
       description:
         "An API backend for a retrieval-augmented generation (RAG) chatbot, allowing users to upload documents and chat with them.",
       primaryUrl: "https://github.com/remon-rakibul/chatbot-demo-api",
-      secondaryUrl: "https://github.com/remon-rakibul/chatbot-demo-api", // Optional, if applicable
+      secondaryUrl: "#", // Optional, if applicable
     },
     {
       title: "Resume Categorizer",
       description:
         "A machine learning model that categorizes resumes into different sub-domains (e.g., HR, Arts, etc.).",
       primaryUrl: "https://github.com/remon-rakibul/resume-categorization-ml",
-      secondaryUrl: "https://github.com/remon-rakibul/resume-categorization-ml", // Optional, if applicable
+      secondaryUrl: "#", // Optional, if applicable
     },
     {
       title: "AriSaf Chatbot",
       description:
         "A chatbot for the AriSaf Tech website that utilizes the knowledge base from the site URLs.",
       primaryUrl: "https://github.com/remon-rakibul/AriSaf-ChatBot",
-      secondaryUrl: "https://github.com/remon-rakibul/AriSaf-ChatBot", // Optional, if applicable
+      secondaryUrl: "#", // Optional, if applicable
     },
     {
       title: "RealEstate Backend",
       description:
         "An API for the backend of a real estate website, developed using Django Rest Framework.",
       primaryUrl: "https://github.com/remon-rakibul/real-estate-backend",
-      secondaryUrl: "https://github.com/remon-rakibul/real-estate-backend", // Optional, if applicable
+      secondaryUrl: "#", // Optional, if applicable
     },
     {
       title: "Book Recommender System",
       description:
         "A system where users can select a book name and receive recommendations for five similar books.",
       primaryUrl: "https://github.com/remon-rakibul/books-recommender-system",
-      secondaryUrl: "https://github.com/remon-rakibul/books-recommender-system", // Optional, if applicable
+      secondaryUrl: "https://books-recommender-system-jad3vmkvta8qd8vc8bzzmm.streamlit.app/", // Optional, if applicable
+    },
+    {
+      title: "Movie Dataset 2024",
+      description: 
+        "A dataset of all movies till 2024, Can be used for EDA, Recommendation Systems, Data Analysis, etc",
+      primaryUrl: "https://github.com/remon-rakibul/movie-dataset-2023",
+      secondaryUrl: "#",
     },
     {
       title: "Covid 19 Detector",
       description:
         "A deep learning application that detects COVID-19 from chest X-ray images, built using the Django framework.",
       primaryUrl: "https://github.com/remon-rakibul/covid-19_django_app",
-      secondaryUrl: "https://github.com/remon-rakibul/covid-19_django_app", // Optional, if applicable
+      secondaryUrl: "#", // Optional, if applicable
     },
   ];
   return (
